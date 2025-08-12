49cf1b1add7b051fbdb04f8302061be72a01d138
moonhoax <dev@julesmrsic.com>
2025-08-12 14:40:04 -0700

    Fix Layout.tsx GTAG

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./css/style.css";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
-import Header from "@/components/ui/header";\
+import Header from "@/components/ui/header";
 import Script from "next/script"; // ✅ You need this import
 
 
